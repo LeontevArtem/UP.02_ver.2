0cd7ef517f84e9f55a575117acc14f35591a4032
artem <artemleont03@gmail.com>
2024-03-05 17:03:28 +0500

    Сделал загрузку в бд страницы Equipment

--- Pages/Equipment/EquipmentEdit.xaml.cs
+++ Pages/Equipment/EquipmentEdit.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DBModule.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -45,6 +46,7 @@ namespace UP._02_ver._2.Pages.Equipment
             Direction.ItemsSource = mainWindow.DirectionsList;
             Model.ItemsSource = mainWindow.ModelsList;
             Type.ItemsSource = mainWindow.EquipmentTypesList;
+            Room.ItemsSource = mainWindow.RoomsList;
 
         }
         public void LoadData(Classes.Equipment curEquipment)
@@ -57,12 +59,54 @@ namespace UP._02_ver._2.Pages.Equipment
             TempUser.SelectedItem = curEquipment.Temp_user;
             Model.SelectedItem = curEquipment.Model;
             Type.SelectedItem = curEquipment.Type;
-            Model.SelectedItem = curEquipment.Model;
             Direction.SelectedItem = curEquipment.Direction;
+            Room.SelectedItem = curEquipment.Room;
         }
         public void BackClick(object sender, MouseButtonEventArgs e)
         {
             mainWindow.OpenPage(ParrentPage);
+        }
+        public void SaveClick(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                if (curEquipment == null)
+                {
+                    System.Data.DataTable UserQuerry = MsSQL.Select($"INSERT INTO [dbo].[Equipment]([Name],[Image],[Room],[User],[Temp_user],[Cost],[Direction],[Model],[Type]) VALUES ('" +
+                        $"{Name.GetText()}','{0}','" +
+                        $"{(Room.SelectedItem as Classes.Rooms).Room_id}','" +
+                        $"{(User.SelectedItem as Classes.Users).User_id}','" +
+                        $"{(TempUser.SelectedItem as Classes.Users).User_id}','" +
+                        $"{Cost.GetText()}','" +
+                        $"{(Direction.SelectedItem as Classes.Directions).Direction_id}','" +
+                        $"{(Model.SelectedItem as Classes.Models).Model_id}','" +
+                        $"{(Type.SelectedItem as Classes.Equipment_types).Type_id}')", 
+                        DBModule.Pages.Settings.ConnectionString);
+                }
+                else
+                {
+                    System.Data.DataTable ProgramsQuerry = MsSQL.Select($"UPDATE [dbo].[Equipment] SET " +
+                        $"[Name] = '{Name.GetText()}'," +
+                        $"[Image] = '{0}'," +
+                        $"[Room] = '{(Room.SelectedItem as Classes.Rooms).Room_id}'," +
+                        $"[User] = '{(User.SelectedItem as Classes.Users).User_id}'," +
+                        $"[Temp_user] = '{(TempUser.SelectedItem as Classes.Users).User_id}'," +
+                        $"[Cost] = '{Cost.GetText()}'," +
+                        $"[Direction] = '{(Direction.SelectedItem as Classes.Directions).Direction_id}'," +
+                        $"[Model] = '{(Model.SelectedItem as Classes.Models).Model_id}'," +
+                        $"[Type] = '{(Type.SelectedItem as Classes.Equipment_types).Type_id}' " +
+                        $"WHERE EquipmentID = '{curEquipment.Equipment_id}'", DBModule.Pages.Settings.ConnectionString);
+                }
+                MessageBox.Show("Успешно");
+                mainWindow.LoadData(0);
+                mainWindow.OpenPage(ParrentPage);
+                (ParrentPage as Pages.Equipment.EquipmentMain).ShowEquipment();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
         }
 
     }
